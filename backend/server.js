3a785323e095e206cae8fbc3eb1647ddfccf1e6e
Stingray <stingraysonggay@gmail.com>
2024-01-18 19:53:55 +0800

    Added post handlers for server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 3000;
+
+app.use(express.static("frontend"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/login.html", (req, res) => {
+  console.log(
+    `LOGIN POST: username=${req.body.username}; password=${req.body.password}`
+  );
+  res.json(req.body);
+});
+
+app.post("/create_account.html", (req, res) => {
+  console.log(
+    `CREATE ACCOUNT POST: username=${req.body.username}; password=${req.body.password}`
+  );
+  res.json(req.body);
 });
 
 app.listen(port, () => {
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,24 +1,34 @@
-function submitLoginForm() {
-  // Add your login logic here
-  alert("Login logic to be implemented");
+function initializeReqOptions(raw, reqType) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  raw = JSON.stringify(raw);
+
+  var requestOptions = {
+    method: reqType,
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  return requestOptions;
 }
 
-function showCreateAccount() {
-  document.getElementById("createAccountForm").style.display = "block";
+function submitForm(formType) {
+  const userAndPass = getUserAndPass();
+  const reqType = "POST";
+  const requestOptions = initializeReqOptions(userAndPass, reqType);
+
+  fetch(`http://localhost:3000/${formType}.html`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
 
-function createAccount() {
-  const newUsername = document.getElementById("newUsername").value;
-  const newPassword = document.getElementById("newPassword").value;
+function getUserAndPass() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-  if (hasEmptyField(newUsername, newPassword)) {
-    alert("Fields should not be empty.");
-    return;
-  }
-
-  console.log("New Username:", newUsername);
-  console.log("New Password:", newPassword);
-  alert("Account creation logic to be implemented");
+  return { username: username, password: password };
 }
 
 function hasEmptyField(username, password) {
