54c4c3b6d0083d2f241e6aac40f19227f5385c07
Stingray <stingraysonggay@gmail.com>
2024-02-08 10:21:50 +0800

    Separate file for middlewares for modularity
    add middleware to home route for continuation of
    auth logic

---
+++ backend/middlewares.js
@@ -0,0 +1,84 @@
+const session = require("express-session");
+const { authUser, hashPassword } = require("./auth_ops");
+const { insertAccountCred } = require("./db_ops");
+const { Pool } = require("pg");
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
+require("dotenv").config();
+
+const password = process.env.DB_PASSWORD;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "Discussion Database",
+  password: password,
+  port: 5432,
+});
+
+const redisClient = redis.createClient();
+redisClient.connect().catch(console.error);
+const redisStore = new RedisStore({
+  client: redisClient,
+});
+
+redisClient.on("error", function (err) {
+  console.log("Could not establish a connection with redis. " + err);
+});
+redisClient.on("connect", function (err) {
+  console.log("Connected to redis successfully");
+});
+
+const sessionMiddleware = session({
+  store: redisStore,
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SECRET,
+});
+
+const authenticate = async (req, res, next) => {
+  try {
+    const auth_res = await authUser(req.body, pool);
+    if (auth_res) {
+      req.session.username = req.body.username;
+      next();
+    } else {
+      console.log("ACCOUNT NOT IN DB");
+      res.status(401).send("Unauthorized");
+    }
+  } catch (error) {
+    console.error("Error in authentication middleware:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+const createAccount = async (req, res, next) => {
+  try {
+    hashPassword(req.body.password, async (err, hashedResult) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      const accountObject = {
+        username: req.body.username,
+        password: hashedResult.hash,
+        salt: hashedResult.salt,
+      };
+
+      const insertionResult = await insertAccountCred(accountObject, pool);
+      console.log(insertionResult);
+
+      next();
+    });
+  } catch (error) {
+    console.error("Error in createAccount middleware:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = {
+  authenticate,
+  sessionMiddleware,
+  createAccount,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,99 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const { authUser, hashPassword } = require("./auth_ops");
-const { insertAccountCred } = require("./db_ops");
-const { Pool } = require("pg");
-const RedisStore = require("connect-redis").default;
-const redis = require("redis");
+const {
+  authenticate,
+  sessionMiddleware,
+  createAccount,
+} = require("./middlewares");
 const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const port = 3000;
-const password = process.env.DB_PASSWORD;
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Discussion Database",
-  password: password,
-  port: 5432,
-});
-const redisClient = redis.createClient();
-redisClient.connect().catch(console.error);
-const redisStore = new RedisStore({
-  client: redisClient,
-});
-
-redisClient.on("error", function (err) {
-  console.log("Could not establish a connection with redis. " + err);
-});
-redisClient.on("connect", function (err) {
-  console.log("Connected to redis successfully");
-});
 
 app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    store: redisStore,
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.SECRET,
-  })
-);
+app.use(sessionMiddleware);
+app.use("/login.html", authenticate);
+app.use("/create_account.html", createAccount);
 
 // Main GET handler
 app.get("/", (req, res) => {
   res.redirect("/login.html");
 });
 
-app.get("/home.html", (req, res) => {});
-
 // Login POST handler
 app.post("/login.html", async (req, res) => {
-  try {
-    authUser(req.body, pool)
-      .then((auth_res) => {
-        if (auth_res) {
-          const session = req.session;
-          session.username = req.body.username;
-          // THINK ABOUT SESSION LOGIC
-          // Create Session Here
-          res.redirect("/home.html");
-          console.log("REDIRECT");
-        } else {
-          console.log("ACCOUNT NOT IN DB");
-        }
-      })
-      .catch((err) => {
-        console.error("Error in login post handler:", err);
-        res.status(500).send("Internal Server Error");
-      });
-  } catch (error) {
-    console.error("Error in login post handler:", error);
-    res.status(500).send("Internal Server Error");
-  }
+  res.redirect("/home.html");
 });
 
 // Create Account POST handler
 app.post("/create_account.html", (req, res) => {
-  hashPassword(req.body.password, async (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const accountObject = {
-        username: req.body.username,
-        password: res.hash,
-        salt: res.salt,
-      };
-
-      const insertion_res = await insertAccountCred(accountObject, pool);
-      console.log(insertion_res);
-    }
-  });
-
   var body = {
     username: req.body.username,
     stat: "Account Created",
